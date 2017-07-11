019bf0196375171bbda00753e6b66188bbe4fec3
Gary Williams <williamsgaryl789@gmail.com>
2017-07-11 15:47:52 -0400

    vending

---
+++ test/test.js
@@ -0,0 +1,32 @@
+const assert = require("assert")
+const request = require("supertest")
+const app = require("../app")
+
+describe("GET /api/customer/items", function () {
+  it("should return successfully", function (done) {
+    request(app)
+      .get("/api/customer/items")
+      .expect(200)
+      .end(done);
+  })
+});
+
+describe("GET /api/vendor/purchase", function () {
+  it("should return successfully", function (done) {
+    request(app)
+      .get("/api/vendor/purchase")
+      .expect(200)
+      .end(done);
+  })
+
+});
+
+
+
+
+
+
+
+
+
+
